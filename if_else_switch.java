76eff49c66731d3eafdba598c3285904e2cb4d4d
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-26 22:15:31 +0530

    if else , else if and switch case

---
+++ if_else_switch.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class if_else_switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Your Age");
+        // int age = sc.nextInt();
+
+        // if (age > 18) {
+        // System.out.println("You are 18 + old");
+        // } else {
+        // System.out.println("You are less than 18");
+        // }
+
+        // System.out.println("Enter Number");
+        // int num = sc.nextInt();
+
+        // if (num % 2 == 0) {
+        // System.out.println("Number is even");
+        // } else {
+        // System.out.println("Number is odd");
+        // }
+
+        // System.out.println("Enter the value of a");
+        // int a = sc.nextInt();
+        // System.out.println("Enter the value of b");
+        // int b = sc.nextInt();
+
+        // if (a == b) {
+        // System.out.println("Both are equal");
+        // } else if (a < b) {
+        // System.out.println("a is less than b");
+        // } else {
+        // System.out.println("a is greater than b");
+        // }
+
+
+
+
+        // System.out.println("Enter eny one number out of 3");
+        // int num = sc.nextInt();
+
+        // switch (num) {
+        //     case 1:
+        //         System.out.println("You have selected 1");
+        //         break;
+        //     case 2:
+        //         System.out.println("You have selected 2");
+        //         break;
+        //     case 3:
+        //         System.out.println("You have selected 3");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid nunber selected");
+        // }
+    }
+
+}
