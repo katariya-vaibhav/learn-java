b1134d6df93d82802b6ad8b5316e42120dca820d
git-demo2 <vaibhavahir4456@gmail.com>
2024-08-03 21:35:24 +0530

    arrays in java

---
+++ arrays.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // int[] arr = new int[2];
+        // System.out.println("Enter the 1 array element");
+        // arr[0] = sc.nextInt();
+        // System.out.println("Enter the 2 array element");
+        // arr[1] = sc.nextInt();
+        // System.out.println(arr[1]);
+
+        // ----------------------------------------------------------------------
+
+        // System.out.println("Enter the size of array");
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
+
+        // for(int i=0; i<n; i++){
+        // System.out.println("Enter the "+(i+1)+" element");
+        // arr[i] = sc.nextInt();
+        // }
+        // for(int i=0; i<n; i++){
+        // System.out.print(arr[i]);
+        // }
+
+        // -----------------------------------------------------------------------
+
+        // System.out.println("Enter the size of array");
+        // int n = sc.nextInt();
+        // String names[] = new String[n];
+        // for(int i=0; i<n; i++){
+        // System.out.println("Enter the "+(i+1)+" element");
+        // names[i] = sc.next();
+        // }
+        // for(int i=0; i<names.length; i++){
+        // System.out.print(names[i] + " ");
+        // }
+
+        // ---------------------------------------------------------------------
+
+        // System.out.println("Size of array");
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     System.out.println("Enter the " + (i + 1) + " element");
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // int min = Integer.MIN_VALUE;
+        // int max = Integer.MAX_VALUE;
+        // System.err.println(min + "  " + max);
+
+        // for(int i=0; i<arr.length; i++){
+        //     if(arr[i]<min){
+        //         min = arr[i];
+        //     }
+        //     if(arr[i]>max){
+        //         max = arr[i];
+        //     }
+        // }
+        // System.out.println("Minimum value is " + min);
+        // System.out.println("Maximum value is " + max);
+
+        // --------------------------------------------------------------------
+        // linear search
+        // System.out.println("Enter the size of array");
+        // int n = sc.nextInt();
+        // int arr[] = new int[n];
+        // for (int i = 0; i < n; i++) {
+        // System.out.println("Enter the " + (i + 1) + " element");
+        // arr[i] = sc.nextInt();
+        // }
+        // for (int i = 0; i < n; i++) {
+        // System.out.print(arr[i] + " ");
+        // }
+
+        // System.out.println("Enter the search number");
+        // int search = sc.nextInt();
+        // for (int i = 0; i < n; i++) {
+        // if (arr[i] == search) {
+        // System.out.println("Element found at index " + i);
+        // break;
+        // }
+        // }
+
+    }
+}
