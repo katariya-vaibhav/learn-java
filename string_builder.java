19ba49c2b7809b0e12ed5a73c074259cb39955ab
git-demo2 <vaibhavahir4456@gmail.com>
2024-08-05 23:55:13 +0530

    StringBuilder methods

--- string_builder.java
+++ string_builder.java
@@ -6,27 +6,42 @@ public class string_builder {
         System.out.println(sb);
 
         // char at 
-        System.out.println(sb.charAt(0));
+        // System.out.println(sb.charAt(0));
         
         // append
-        sb.append(" world");
-        System.out.println(sb);
+        // sb.append(" world");
+        // System.out.println(sb);
 
         // set char at 
-        sb.setCharAt(0, 'H');
-        System.out.println(sb);
+        // sb.setCharAt(0, 'H');
+        // System.out.println(sb);
 
         // insert
-        sb.insert(11, "!");
-        System.out.println(sb);
+        // sb.insert(11, "!");
+        // System.out.println(sb);
 
         // delete
-        sb.delete(11, 12);
-        System.out.println(sb);
+        // sb.delete(11, 12);
+        // System.out.println(sb);
         
         // reverse
         // sb.reverse();
         // System.out.println(sb);
+
+
+        // reverse
+        // for(int i = 0; i < sb.length()/2; i++){
+        //     int front = i;
+        //     int back = sb.length() - 1 - i; // 5 - 1 - 0 = 4
+
+        //     int frontChar = sb.charAt(front);
+        //     int backChar = sb.charAt(back);
+            
+        //     sb.setCharAt(front, (char) backChar);
+        //     sb.setCharAt(back, (char) frontChar);
+        // }
+        // System.out.println(sb);
+        
         
         //substring
         // System.out.println(sb.substring(1, 4));
@@ -48,6 +63,7 @@ public class string_builder {
         // trimToSize
         // sb.trimToSize();
         // System.out.println(sb.length());
+
 
     }
 }
