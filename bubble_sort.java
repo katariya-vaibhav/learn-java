94e1ed02a551d9e7f86ba80e374e31097e24e416
git-demo2 <vaibhavahir4456@gmail.com>
2024-08-07 19:31:51 +0530

    bubble sort in java

---
+++ bubble_sort.java
@@ -0,0 +1,23 @@
+public class bubble_sort{
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args){
+        int arr[] = {7,8,2,5,1,4};
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
