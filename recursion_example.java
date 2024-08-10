2dec499cbb24479176b33c3f7a1b5e61922b52ad
git-demo2 <vaibhavahir4456@gmail.com>
2024-08-10 16:26:37 +0530

    isArraySorted , findOccurrences  , moveElements, subsequences program

--- recursion_example.java
+++ recursion_example.java
@@ -1,15 +1,15 @@
-public class recursion_example{
-    public static void towerOfHanoi(int n , String src ,String helper , String destination){
+public class recursion_example {
+    public static void towerOfHanoi(int n, String src, String helper, String destination) {
         if (n == 1) {
-            System.out.println("disk transformation " + src + " to "+ destination);
+            System.out.println("disk transformation " + src + " to " + destination);
             return;
         }
-        towerOfHanoi(n-1, src ,destination, helper);
-        System.out.println("disk transformation " + src + " to "+ destination);
-        towerOfHanoi(n-1, helper, src, destination);
+        towerOfHanoi(n - 1, src, destination, helper);
+        System.out.println("disk transformation " + src + " to " + destination);
+        towerOfHanoi(n - 1, helper, src, destination);
     }
 
-    public static void reverseString(String str , int idx){
+    public static void reverseString(String str, int idx) {
 
         if (idx == 0) {
             System.out.println(str.charAt(idx));
@@ -17,11 +17,84 @@ public class recursion_example{
         }
 
         System.out.println(str.charAt(idx));
-        reverseString(str, idx-1);
+        reverseString(str, idx - 1);
     }
+
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void findOccurrences(String str, int idx, char element) {
+
+        if (idx == str.length()) {
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+
+        char currentElement = str.charAt(idx);
+        if (currentElement == element) {
+            if (first == -1) {
+                first = idx;
+            } else {
+                last = idx;
+            }
+        }
+        findOccurrences(str, idx + 1, currentElement);
+    }
+
+    public static boolean isArraySorted(int arr[], int idx) {
+
+        if (idx == arr.length - 1) {
+            return true;
+        }
+
+        if (arr[idx] < arr[idx + 1]) {
+            return isArraySorted(arr, idx + 1);
+        } else {
+            return false;
+        }
+    }
+
+    public static void moveStringElement(String str, int idx, String newString, int count, char element) {
+
+        if (idx == str.length()) {
+            for (int i = 0; i < count; i++) {
+                newString += element;
+            }
+            System.out.println(newString);
+            return;
+        }
+
+        char currentElement = str.charAt(idx);
+        if (currentElement == element) {
+            count++;
+            moveStringElement(str, idx + 1, newString, count, currentElement);
+        } else {
+            newString += currentElement;
+            moveStringElement(str, idx + 1, newString, count, element);
+        }
+    }
+
+
+    public static void subsequences(String str, int idx , String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char c = str.charAt(idx);
+        subsequences(str, idx+1, newString+c);
+        subsequences(str, idx+1, newString);
+        
+    }
+
     public static void main(String[] args) {
         // towerOfHanoi(3, "S", "H", "D");
         // String str = "Hello World";
         // reverseString(str, str.length()-1);
+        // findOccurrences("aabsddassac", 0, 'a');
+        // int arr[] = { 1 ,2,3};
+        // System.out.println(isArraySorted(arr, 0));
+        // moveStringElement("helllloword", 0, "", 0, 'l');
+        // subsequences("abc", 0, "");
     }
 }
