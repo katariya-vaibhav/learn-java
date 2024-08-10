99993529b2f4ff1a296cb29034447edfa35cf087
git-demo2 <vaibhavahir4456@gmail.com>
2024-08-10 15:38:19 +0530

    tower of hanoi and reverse a string program

--- recursion_example.java
+++ recursion_example.java
@@ -1,5 +1,27 @@
 public class recursion_example{
+    public static void towerOfHanoi(int n , String src ,String helper , String destination){
+        if (n == 1) {
+            System.out.println("disk transformation " + src + " to "+ destination);
+            return;
+        }
+        towerOfHanoi(n-1, src ,destination, helper);
+        System.out.println("disk transformation " + src + " to "+ destination);
+        towerOfHanoi(n-1, helper, src, destination);
+    }
+
+    public static void reverseString(String str , int idx){
+
+        if (idx == 0) {
+            System.out.println(str.charAt(idx));
+            return;
+        }
+
+        System.out.println(str.charAt(idx));
+        reverseString(str, idx-1);
+    }
     public static void main(String[] args) {
-        
+        // towerOfHanoi(3, "S", "H", "D");
+        // String str = "Hello World";
+        // reverseString(str, str.length()-1);
     }
 }
