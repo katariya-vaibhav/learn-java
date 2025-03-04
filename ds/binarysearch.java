ef1a59e7a8b2f911260b7edcf2f0ceebd28cd7a4
_coding__hub_ <143187581+katariya-vaibhav@users.noreply.github.com>
2025-03-04 17:00:29 +0530

    refactor BinarySearch class name to lowercase and format array initialization

--- ds/binarysearch.java
+++ ds/binarysearch.java
@@ -1,4 +1,4 @@
-public class BinarySearch {
+public class binarysearch {
     // Recursive Binary Search
     public static int binarySearch(int[] arr, int left, int right, int target) {
         if (left > right) {
@@ -11,18 +11,18 @@ public class BinarySearch {
         if (arr[mid] == target) {
             return mid;
         }
-        
+
         // If target is smaller, search in the left half
         if (target < arr[mid]) {
             return binarySearch(arr, left, mid - 1, target);
         }
-        
+
         // Else, search in the right half
         return binarySearch(arr, mid + 1, right, target);
     }
 
     public static void main(String[] args) {
-        int[] sortedArray = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+        int[] sortedArray = { 2, 5, 8, 12, 16, 23, 38, 45, 56, 72 };
         int target = 23;
 
         int result = binarySearch(sortedArray, 0, sortedArray.length - 1, target);
