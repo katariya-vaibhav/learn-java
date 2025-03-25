020da5afb7c027279e61a76cfd855ca0a261a8d5
_coding__hub_ <143187581+katariya-vaibhav@users.noreply.github.com>
2025-03-25 15:00:59 +0530

    add demo class with threaded execution example

---
+++ clg/demo.java
@@ -0,0 +1,22 @@
+
+public class demo {
+
+    class thread extends Thread {
+        public void run() {
+            try {
+                for (int i = 5; i > 0; i--) {
+                    System.out.println("run " + ": " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        demo outer = new demo();
+        thread t1 = outer.new thread();
+        t1.start();
+    }
+}
