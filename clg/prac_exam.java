25b7596f3af51d3d9bbca275d25135026fa7fae3
_coding__hub_ <143187581+katariya-vaibhav@users.noreply.github.com>
2025-04-26 14:52:11 +0530

    add string manipulation classes for vowel extraction and reversal

---
+++ clg/prac_exam.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class main {
+
+    // Class to find and print vowels
+    public static class FindVowel {
+        FindVowel(String str) {
+            System.out.println("Original String: " + str);
+            printVowels(str);
+        }
+
+        void printVowels(String str) {
+            System.out.print("Vowels in the string: ");
+            for (char ch : str.toLowerCase().toCharArray()) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    System.out.print(ch + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Class to reverse the string
+    public static class ReverseStr {
+        ReverseStr(String str) {
+            System.out.println("Original String (before reversing): " + str);
+            reverseString(str);
+        }
+
+        void reverseString(String str) {
+            StringBuffer sb = new StringBuffer(str);
+            System.out.println("Reversed String: " + sb.reverse());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your String:");
+        String userStr = sc.nextLine();
+
+        System.out.println("Length of String: " + userStr.length());
+        System.out.println("UpperCase of String: " + userStr.toUpperCase());
+
+        FindVowel fv = new FindVowel(userStr);
+        ReverseStr rv = new ReverseStr(userStr);
+
+        sc.close(); // Close the scanner
+    }
+}
